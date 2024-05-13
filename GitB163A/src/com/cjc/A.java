367374b393be0b069212a9cbd6d2ad2044048dc8
anishchavan <anishchavan29@gmail.com>
2024-05-13 15:10:52 +0530

    commit changes

--- GitB163A/src/com/cjc/A.java
+++ GitB163A/src/com/cjc/A.java
@@ -1,6 +1,9 @@
 package com.cjc;
 
 public class A {
+	public void m1() {
+		System.out.println("m1 method");
+	}
 	public static void main(String[] args) {
 		System.out.println("This is Git demo");
 	}
