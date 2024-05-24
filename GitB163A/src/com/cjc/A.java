4c014ece9aeea9cef5fe5933c4a9fb301f0c5f82
Anish Chavan <anishchavan29@gmail.com>
2024-05-24 09:45:21 +0530

    By

--- GitB163A/src/com/cjc/A.java
+++ GitB163A/src/com/cjc/A.java
@@ -8,6 +8,11 @@ public class A {
 	public void m2() {
 		System.out.printnln("m1 method");
 	}
+
+	public void m3(){
+		System.out.println("m3 method");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("This is Git demo");
 	}
